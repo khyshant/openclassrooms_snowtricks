272c82866a323e523651e10a2f5609adea105f0f
“khyshant” <anth.blanchard@gmail.com>
2022-12-09 22:10:23 +0100

    error on git need rebase?

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 // assets/app.js
-=======
->>>>>>> 30e168abb5b7f1383316b18414b6422864194ccb
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -9,16 +6,8 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-<<<<<<< HEAD
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
-=======
-// any CSS you import will output into a single css file (app.scss in this case)
-import './styles/app.scss';
-
-// start the Stimulus application
-import './bootstrap';
->>>>>>> 30e168abb5b7f1383316b18414b6422864194ccb
--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,23 +1,12 @@
-<<<<<<< HEAD
 // assets/bootstrap.js
-=======
->>>>>>> 30e168abb5b7f1383316b18414b6422864194ccb
 import { startStimulusApp } from '@symfony/stimulus-bridge';
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context(
     '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
     true,
-<<<<<<< HEAD
     /\.(j|t)sx?$/
 ));
 
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
-=======
-    /\.[jt]sx?$/
-));
-
-// register any custom, 3rd party controllers here
-// app.register('some_controller_name', SomeImportedController);
->>>>>>> 30e168abb5b7f1383316b18414b6422864194ccb
